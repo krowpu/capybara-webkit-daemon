8ccaacd7591cd21ad3cef17749f84e880f190010
krowpu <krowpu@tightmail.com>
2017-03-14 22:04:34 +0500

    Add browser messaging methods

--- lib/capybara/webkit/daemon/client/browser.rb
+++ lib/capybara/webkit/daemon/client/browser.rb
@@ -3,11 +3,22 @@
 require 'capybara'
 require 'capybara/webkit/browser'
 
+require 'capybara/webkit/daemon/common'
+
 module Capybara
   module Webkit
     module Daemon
       module Client
         class Browser < Capybara::Webkit::Browser
+        private
+
+          def message(s)
+            @connection.print "#{Common::START_CHR}#{s}#{Common::END_CHR}"
+          end
+
+          def message_binary(s)
+            @connaction.print "#{Common::HEADER_CHR}#{s.length}#{Common::START_CHR}#{s}#{Common::END_CHR}"
+          end
         end
       end
     end
