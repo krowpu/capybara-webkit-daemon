e08d68d946bc362a5a74f431f5a3dd314226cec3
krowpu <krowpu@tightmail.com>
2017-03-14 16:51:24 +0500

    Some refactoring

--- lib/capybara/webkit/daemon/server/link.rb
+++ lib/capybara/webkit/daemon/server/link.rb
@@ -16,22 +16,8 @@ module Capybara
           end
 
           def start
-            extractor_thread = Thread.start do
-              begin
-                extractor.round until @terminating
-              rescue EOFError
-                @terminating = true
-              end
-            end
-
-            inserter_thread = Thread.start do
-              begin
-                inserter.round until @terminating
-              rescue EOFError
-                @terminating = true
-              end
-            end
-
+            extractor_thread
+            inserter_thread
           ensure
             extractor_thread&.join
             inserter_thread&.join
@@ -39,6 +25,28 @@ module Capybara
 
           def terminate!
             @terminating = true
+          end
+
+        private
+
+          def extractor_thread
+            @extractor_thread ||= Thread.start do
+              begin
+                extractor.round until @terminating
+              rescue EOFError
+                @terminating = true
+              end
+            end
+          end
+
+          def inserter_thread
+            @inserter_thread ||= Thread.start do
+              begin
+                inserter.round until @terminating
+              rescue EOFError
+                @terminating = true
+              end
+            end
           end
         end
       end
