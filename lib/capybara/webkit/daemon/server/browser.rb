8c15ff27cf03275976757feb822578bdf4218149
krowpu <krowpu@tightmail.com>
2017-03-14 20:56:27 +0500

    Browser creates connection

--- lib/capybara/webkit/daemon/server/browser.rb
+++ lib/capybara/webkit/daemon/server/browser.rb
@@ -2,11 +2,23 @@
 
 require 'capybara/webkit/browser'
 
+require 'capybara/webkit/daemon/server/connection'
+
 module Capybara
   module Webkit
     module Daemon
       module Server
         class Browser < Capybara::Webkit::Browser
+          attr_reader :configuration
+
+          def initialize(configuration:)
+            @configuration = configuration
+            connection
+          end
+
+          def connection
+            @connection ||= Connection.new configuration: configuration
+          end
         end
       end
     end
