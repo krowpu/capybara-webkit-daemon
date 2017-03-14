1353ba69920d720292ac1148b1487e186d2d1fbc
krowpu <krowpu@tightmail.com>
2017-03-14 21:01:25 +0500

    Session creates browser

--- lib/capybara/webkit/daemon/server/session.rb
+++ lib/capybara/webkit/daemon/server/session.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
+require 'capybara/webkit/daemon/server/browser'
+
 module Capybara
   module Webkit
     module Daemon
       module Server
         class Session
           attr_reader :client
+          attr_reader :configuration
 
-          def initialize(client)
+          def initialize(client, configuration:)
             @client = client
+            @configuration = configuration
+            browser
+          end
+
+          def browser
+            @browser ||= Browser.new configuration: configuration
           end
         end
       end
