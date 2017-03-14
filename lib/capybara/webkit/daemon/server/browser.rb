c06faf5a79e88f2c7f2d9bcaaf1f13cf29de5ac6
krowpu <krowpu@tightmail.com>
2017-03-14 20:29:26 +0500

    Add class Capybara::Webkit::Daemon::Server::Browser

---
+++ lib/capybara/webkit/daemon/server/browser.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'capybara/webkit/browser'
+
+module Capybara
+  module Webkit
+    module Daemon
+      module Server
+        class Browser < Capybara::Webkit::Browser
+        end
+      end
+    end
+  end
+end
