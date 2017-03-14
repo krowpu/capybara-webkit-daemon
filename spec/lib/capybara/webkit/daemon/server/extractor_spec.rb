1862bd49aba30ec830ee1f587f0de5f9b8bf8eeb
krowpu <krowpu@tightmail.com>
2017-03-14 17:26:16 +0500

    Add specs for Capybara::Webkit::Daemon::Server::Extractor

---
+++ spec/lib/capybara/webkit/daemon/server/extractor_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'capybara/webkit/daemon/server/extractor'
+
+RSpec.describe Capybara::Webkit::Daemon::Server::Extractor do
+  subject { described_class.new source: source, destination: destination }
+
+  let(:source) { StringIO.new }
+  let(:destination) { StringIO.new }
+
+  def input(s)
+    orig = source.pos
+    source.seek 0, IO::SEEK_END
+    source.print s
+    source.seek orig
+    subject.round
+  end
+
+  def output
+    orig = destination.pos
+    destination.seek 0
+    result = destination.read
+    destination.seek orig
+    result
+  end
+
+  describe '#round' do
+    context 'when got some messages' do
+      let(:msg) { 'Hello, World!' }
+
+      it 'extracts message' do
+        expect(subject).to receive(:extracted).with(msg)
+        input "123\x02#{msg}\x03456"
+        expect(output).to eq '123456'
+      end
+    end
+
+    context 'when no messages got' do
+      it 'transfers data as is' do
+        input '123'
+        expect(output).to eq '123'
+      end
+    end
+  end
+end
