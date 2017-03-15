97875e73746d597c2800691c84016118139a452e
krowpu <krowpu@tightmail.com>
2017-03-15 23:21:26 +0500

    Remove unnecessary code

--- lib/capybara/webkit/daemon/client/browser.rb
+++ lib/capybara/webkit/daemon/client/browser.rb
@@ -10,15 +10,6 @@ module Capybara
     module Daemon
       module Client
         class Browser < Capybara::Webkit::Browser
-        private
-
-          def message(s)
-            @connection.print "#{Common::START_CHR}#{s}#{Common::END_CHR}"
-          end
-
-          def message_binary(s)
-            @connaction.print "#{Common::HEADER_CHR}#{s.length}#{Common::START_CHR}#{s}#{Common::END_CHR}"
-          end
         end
       end
     end
