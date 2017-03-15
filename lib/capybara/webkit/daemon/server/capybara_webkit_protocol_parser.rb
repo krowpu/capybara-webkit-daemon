dc04b7fa36159150d8dab21e36f4edd722e4b4f4
krowpu <krowpu@tightmail.com>
2017-03-15 19:18:25 +0500

    Declare states

--- lib/capybara/webkit/daemon/server/capybara_webkit_protocol_parser.rb
+++ lib/capybara/webkit/daemon/server/capybara_webkit_protocol_parser.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
+require 'aasm'
+
 module Capybara
   module Webkit
     module Daemon
       module Server
         class CapybaraWebkitProtocolParser
+          include AASM
+
+          aasm do
+            state :name, initial: true
+            state :args_count
+            state :arg_size
+            state :arg
+          end
+
           def call(s)
             s
           end
