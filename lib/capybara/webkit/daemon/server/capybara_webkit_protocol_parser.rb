da84b8ab6247a032da9031e915ef2bdde4991ce2
krowpu <krowpu@tightmail.com>
2017-03-15 19:55:13 +0500

    Some refactoring

--- lib/capybara/webkit/daemon/server/capybara_webkit_protocol_parser.rb
+++ lib/capybara/webkit/daemon/server/capybara_webkit_protocol_parser.rb
@@ -10,27 +10,27 @@ module Capybara
           include AASM
 
           aasm do
-            state :name, initial: true
-            state :args_count
-            state :arg_size
-            state :arg
+            state :name, initial: true, before_enter: :getting_name,       after_exit: :got_name
+            state :args_count,          before_enter: :getting_args_count, after_exit: :got_args_count
+            state :arg_size,            before_enter: :getting_arg_size,   after_exit: :got_arg_size
+            state :arg,                 before_enter: :getting_arg,        after_exit: :got_arg
 
-            event :newline, after: :got_name do
+            event :newline do
               transitions from: :name, to: :args_count
             end
 
-            event :newline, after: :got_args_count do
+            event :newline do
               transitions from: :args_count, to: :name, guard: :zero_args?
               transitions from: :args_count, to: :arg_size
             end
 
-            event :newline, after: :got_arg_size do
+            event :newline do
               transitions from: :arg_size, to: :name, guards: %i(empty_arg? last_arg?)
               transitions from: :arg_size, to: :arg_size, guard: :empty_arg?
               transitions from: :arg_size, to: :arg
             end
 
-            event :arg_ended, after: :got_arg do
+            event :arg_ended do
               transitions from: :arg, to: :name, guard: :last_arg?
               transitions from: :arg, to: :arg_size
             end
@@ -41,6 +41,38 @@ module Capybara
           end
 
         private
+
+          def getting_name
+            @name = ''
+          end
+
+          def got_name
+            @name = nil
+          end
+
+          def getting_args_count
+            @args_count = ''
+          end
+
+          def got_args_count
+            @args_count = nil
+          end
+
+          def getting_arg_size
+            @arg_size = ''
+          end
+
+          def got_arg_size
+            @arg_size = nil
+          end
+
+          def getting_arg
+            @arg = ''
+          end
+
+          def got_arg
+            @arg = nil
+          end
 
           def zero_args?
             args_count.zero?
