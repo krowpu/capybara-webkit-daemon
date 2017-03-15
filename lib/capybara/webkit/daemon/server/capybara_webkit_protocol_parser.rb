975517f33955d2e7666f997c3fa4a570c848e7ba
krowpu <krowpu@tightmail.com>
2017-03-16 01:48:48 +0500

    Implement

--- lib/capybara/webkit/daemon/server/capybara_webkit_protocol_parser.rb
+++ lib/capybara/webkit/daemon/server/capybara_webkit_protocol_parser.rb
@@ -7,12 +7,120 @@ module Capybara
         class CapybaraWebkitProtocolParser
           def initialize(&block)
             @block = block
+            @state = Name.new
+            @result = ''
           end
 
           def call(s)
             return s if @block.nil?
-            return s unless @block&.(s)
-            ''
+
+            s.each_char do |c|
+              @state = @state.(c, &cb)
+            end
+
+            result = @result
+            @result = ''
+            result
+          end
+
+          def cb
+            @cb ||= method :command
+          end
+
+          def command(name, args)
+            @result += serialize name, args if @block.nil? || !@block.(name, args)
+          end
+
+          def serialize(name, args)
+            "#{name}\n#{args.count}\n#{args.map { |arg| "#{arg.bytesize}\n#{arg}" }.join}"
+          end
+
+          class State
+            def call(_c)
+              raise NotImplementedError, "#{self.class}#call"
+            end
+          end
+
+          class Name < State
+            def initialize
+              @name = ''
+            end
+
+            def call(c)
+              return ArgsCount.new @name if c == "\n"
+              @name += c
+              self
+            end
+          end
+
+          class ArgsCount < State
+            def initialize(name)
+              raise 'name can not be empty' if name.empty?
+              @name = name
+              @args_count = ''
+            end
+
+            def call(c)
+              if c == "\n"
+                if @args_count == '0'
+                  yield @name, []
+                  return Name.new
+                end
+                return ArgSize.new @name, @args_count
+              end
+
+              @args_count += c
+              self
+            end
+          end
+
+          class ArgSize < State
+            def initialize(name, args_count, args: [], arg_size: '')
+              @name = name
+              @args_count = Integer args_count
+              @args = args
+              @arg_size = arg_size
+            end
+
+            def call(c)
+              if c == "\n"
+                if @arg_size == '0'
+                  if @args.count == @args_count
+                    yield @name, @args
+                    return Name.new
+                  end
+                  return ArgSize.new @name, @args_count, args: @args
+                end
+                return Arg.new @name, @args_count, @args, @arg_size
+              end
+
+              @arg_size += c
+              self
+            end
+          end
+
+          class Arg < State
+            def initialize(name, args_count, args, arg_size)
+              @name = name
+              @args_count = args_count
+              @args = args
+              @arg_size = Integer arg_size
+              @arg = ''
+            end
+
+            def call(c)
+              if @arg.size == @arg_size
+                @args << @arg
+                if @args.count == @args_count
+                  yield @name, @args
+                  return Name.new
+                end
+                return ArgSize.new @name, @args_count, args: @args, arg_size: c
+              end
+
+              @arg += c
+              self
+            end
           end
         end
       end
