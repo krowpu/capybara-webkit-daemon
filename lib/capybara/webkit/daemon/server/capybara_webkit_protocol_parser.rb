b7354f9c8b850591ff19f0c9f7f64406fcf3177c
krowpu <krowpu@tightmail.com>
2017-03-15 19:26:41 +0500

    Declare guards

--- lib/capybara/webkit/daemon/server/capybara_webkit_protocol_parser.rb
+++ lib/capybara/webkit/daemon/server/capybara_webkit_protocol_parser.rb
@@ -18,16 +18,16 @@ module Capybara
             event :newline do
               transitions from: :name, to: :args_count
 
-              transitions from: :args_count, to: :name
+              transitions from: :args_count, to: :name, guard: :zero_args?
               transitions from: :args_count, to: :arg_size
 
-              transitions from: :arg_size, to: :name
-              transitions from: :arg_size, to: :arg_size
+              transitions from: :arg_size, to: :name, guards: %i(empty_arg? last_arg?)
+              transitions from: :arg_size, to: :arg_size, guard: :empty_arg?
               transitions from: :arg_size, to: :arg
             end
 
             event :arg_ended do
-              transitions from: :arg, to: :name
+              transitions from: :arg, to: :name, guard: :last_arg?
               transitions from: :arg, to: :arg_size
             end
           end
